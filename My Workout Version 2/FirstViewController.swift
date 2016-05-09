822b142ddba3c2f1bdbc161b265377494d04e9de
Rosie Crisman <16rcrisman@castilleja.org>
2016-05-08 22:16:06 -0700

    Initial Commit

---
+++ My Workout Version 2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  My Workout Version 2
+//
+//  Created by rosiecrisman on 5/8/16.
+//  Copyright © 2016 rosiecrisman. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
